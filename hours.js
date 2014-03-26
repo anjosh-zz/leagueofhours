7b46e4e871d65d1e4deb96f6d69f3cba3bd2e58b
anjosh <joshuaan1@gmail.com>
2014-03-25 23:17:46 -0400

    calculated total hours with 30 minutes estimate of avg game time

--- hours.js
+++ hours.js
@@ -18,14 +18,14 @@ var FirstBlood1x1AvgTime = 5;
 var FirstBlood2x2AvgTime = 10;
 var SummonersRift6x6AvgTime = 35;
 
-//$(document).ready(function() {
-//  var progressBar = $('#progress-bar');
-//  progressBar.on("transitionend", showHoursHideBar);
-//  $('#recalculate').on("click", showFromHideHours);
-//});
+$(document).ready(function() {
+  var progressBar = $('#progress-bar');
+  progressBar.on("transitionend", showHoursHideBar);
+  $('#recalculate').on("click", showFromHideHours);
+});
 
 function getHours(region, summonerName) {
-  //showBarHideForm();
+  showBarHideForm();
   region = region.value.toLowerCase();
   summonerName = summonerName.value.toLowerCase();
 
@@ -43,43 +43,67 @@ function getHours(region, summonerName) {
   var season = "SEASON4";
   var s4Stats = getSummonerStats(region, summonerId, season);
 
-  // Set total hours based on Season 4 stats
-  var s4WinsLosses = getWinsLosses(s4Stats);
-  console.log(s4WinsLosses);
-
-  var s4Hours = getTotalHours(s4WinsLosses);
-
   // Get stats for Season 3
   season = "SEASON3";
   var s3Stats = getSummonerStats(region, summonerId, season);
 
-  // Add hours based on Season 3 stats to total hours
-  var s3WinsLosses = getWinsLosses(s3Stats);
-  console.log(s3WinsLosses);
+  // Get total wins and losses based on stats
+  var winsLosses = getWinsLosses(s3Stats, s4Stats);
+  console.log(winsLosses);
 
-  var s3Hours = getTotalHours(s3WinsLosses);
-
-  totalHours = s3Hours + s4Hours;
-  //$('#totalHours').text(Math.round(totalHours.toString()));
+  // Get total hours based on wins and losses
+  var totalHours = getTotalHours(winsLosses);
+  console.log(totalHours);
+  $('#hours').text(Math.round(totalHours.toString()));
 
   return false;
 }
 
 function getTotalHours(winsLosses) {
-  
+  var minutes = 0;
 
+  //for (var i = 0; i < winsLosses.wins.length; i++) {
+  //  minutes += (winsLosses.wins[i] + winsLosses.losses[i]) * 30;
+  //  console.log(minutes);
+  //}
+
+  for (var win in winsLosses.wins) {
+    minutes += winsLosses.wins[win] * 30;
+    console.log(minutes);
+  }
+
+  for (var loss in winsLosses.loss) {
+    minutes += winsLosses.loss[loss] * 30;
+    console.log(minutes);
+  }
+
+  return minutes/60;
 }
 
-function getWinsLosses(stats) {
+function getWinsLosses(stats, newStats) {
   var winsLosses = new Object();
   var wins = new Object();
   var losses = new Object();
 
-  for (var i = 0; i < stats.length; i++) {
-    curr = stats[i];
-    type = curr.playerStatSummaryType;
-    wins[type] = curr.wins;
-    losses[type] = curr.losses ? curr.losses : curr.wins;
+  for (var i = 0; i < newStats.length; i++) {
+    type = newStats[i].playerStatSummaryType;
+    console.log(type);
+
+    wins[type] = newStats[i].wins;
+    console.log(wins[type]);
+    losses[type] = newStats[i].losses ? newStats[i].losses : newStats[i].wins;
+    console.log(losses[type]);
+
+    isRanked = type.substring(0, 6) === "Ranked";
+    console.log(isRanked)
+    if (isRanked) {
+      wins[type] += stats[i].wins;
+      //console.log(stats[i].wins);
+      //console.log(wins[type]);
+      losses[type] += stats[i].losses ? stats[i].losses : stats[i].wins;
+      //console.log(stats[i].losses)
+      //console.log(losses[type]);
+    }
   }
 
   winsLosses.wins = wins;
